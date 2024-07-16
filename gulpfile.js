442a113e93f913d5555f9fffd412ee69c29be098
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2024-07-16 09:24:24 -0700

    refactor: build & add dev script
    - add dev package script w/ hot reloading
    - add prisma build step

--- gulpfile.js
+++ gulpfile.js
@@ -20,8 +20,8 @@ const SOURCEMAPS = MODE === 'development' && process.env.SOURCEMAPS === 'true'
 // --- functions ---
 
 /**
- * Delete files
- * @param {string[]} paths
+ * delete files
+ * @param {string[]} paths  file paths
  * @returns {Promise<void>}
  */
 const rm = async (paths) => {
@@ -32,7 +32,80 @@ const rm = async (paths) => {
   )
 }
 
+/**
+ * @typedef {Object} SpawnPromise
+ * @property {import('child_process').ChildProcess} child  child process
+ * @property {Promise<void>} promise                       exec promise
+ */
+
+/**
+ * spawn promise
+ * @param {string} command                                 spawn command
+ * @param {string[]} args                                  spawn command arguments
+ * @param {import('child_process').SpawnOptions} options   spawn options
+ * @returns {SpawnPromise}                                 spawn promise object
+ */
+const spawnp = (command, args, options) => {
+  const child = spawn(command, args, options)
+  return {
+    child,
+    promise: new Promise((resolve, reject) => {
+      let error = null
+      child
+        .on('error', (err) => {
+          error = err
+        })
+        .on('exit', () => {
+          if (error === null) {
+            resolve()
+          } else {
+            reject(error)
+          }
+        })
+    }),
+  }
+}
+
+/**
+ * start application
+ * @returns {Promise<void>}  application exit promise
+ */
+const run = async () => {
+  const { child, promise } = spawnp(
+    'node',
+    [
+      '--import',
+      'data:text/javascript,import { register } from "node:module"; import { pathToFileURL } from "node:url"; register("ts-node/esm", pathToFileURL("./"));',
+      '-r',
+      'dotenv/config',
+      path.resolve('./src/index.ts'),
+    ],
+    {
+      env: {
+        ...process.env,
+        NODE_ENV: 'development',
+        TS_NODE: 'true',
+      },
+      stdio: 'inherit',
+    },
+  )
+  run.child?.kill()
+  run.child = child
+  return promise
+}
+run.child = null
+
 // --- sub-tasks ---
+
+gulp.task('build:copy', () => {
+  return gulp
+    .src(['assets/**/*', 'config/**/*'], { cwd: './src', cwdbase: true })
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('build:prisma', () => {
+  return spawnp('node', ['./node_modules/prisma/build/index.js', 'generate']).promise
+})
 
 gulp.task('build:typescript', () => {
   const project = ts.createProject('./tsconfig.json', {
@@ -49,43 +122,23 @@ gulp.task('build:typescript', () => {
     .pipe(gulp.dest('./dist'))
 })
 
-gulp.task('build:copy', () => {
-  return gulp
-    .src(['assets/**/*', 'config/**/*'], { cwd: './src', cwdbase: true })
-    .pipe(gulp.dest('./dist'))
-})
-
-gulp.task('start:run', (cb) => {
-  const child = spawn(
-    'node',
-    [
-      '--import',
-      'data:text/javascript,import { register } from "node:module"; import { pathToFileURL } from "node:url"; register("ts-node/esm", pathToFileURL("./"));',
-      '-r',
-      'dotenv/config',
-      './src/index.ts',
-    ],
-    {
-      env: {
-        ...process.env,
-        NODE_ENV: 'development',
-        TS_NODE: 'true',
-      },
-      stdio: 'inherit',
-    },
-  )
-  let error
-  child
-    .on('error', (err) => {
-      error = err
-    })
-    .on('exit', () => cb(error))
-})
-
 // --- entrypoints ---
 
-gulp.task('start', gulp.series('start:run'))
+gulp.task('start', run)
+
+gulp.task('dev', () => {
+  return gulp
+    .watch(
+      ['./src/**/*'],
+      {
+        ignored: ['./src/assets/**/*', './src/test/**/*', './src/types/**/*'],
+        ignoreInitial: false,
+      },
+      gulp.series('build:prisma', run),
+    )
+    .on('change', run)
+})
 
 gulp.task('clean', async () => await rm(['./dist']))
 
-gulp.task('build', gulp.parallel('build:copy', 'build:typescript'))
+gulp.task('build', gulp.series('build:prisma', gulp.parallel('build:copy', 'build:typescript')))
